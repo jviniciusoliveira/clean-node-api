d1b0d2194e1db8be41139af642866bc09bd18e49
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-07-04 10:11:27 -0300

    refactor: move sut creation to a feactory helper method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,15 +1,30 @@
 import { DbAddAccount } from './db-add-account'
+import { Encrypter } from '../../protocols/encrypter'
+
+interface SutTypes {
+  sut: DbAddAccount
+  encripterStub: Encrypter
+}
+
+const makeSut = (): SutTypes => {
+  class EncrypterStub {
+    async encrypt (value: string): Promise<string> {
+      return new Promise(resolve => resolve('hashed_password'))
+    }
+  }
+
+  const encripterStub = new EncrypterStub()
+  const sut = new DbAddAccount(encripterStub)
+
+  return {
+    sut,
+    encripterStub
+  }
+}
 
 describe('DbAddAccount UseCase', () => {
   test('should call Encrypter with corret password', async () => {
-    class EncrypterStub {
-      async encrypt (value: string): Promise<string> {
-        return new Promise(resolve => resolve('hashed_password'))
-      }
-    }
-
-    const encripterStub = new EncrypterStub()
-    const sut = new DbAddAccount(encripterStub)
+    const { sut, encripterStub } = makeSut()
     const encryptSpy = jest.spyOn(encripterStub, 'encrypt')
     const accountData = {
       name: 'valid_name',
