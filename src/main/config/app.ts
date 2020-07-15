cc62268bda41ccfd8929954bb6446df8970bab5c
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-07-15 19:10:38 -0300

    feat: add routes setup

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import setupMiddleware from './middlewares'
+import setupRoute from './routes'
 
 const app = express()
 setupMiddleware(app)
+setupRoute(app)
 
 export default app
