4b9603b4d050d3eef9a01768b1c94444ad1a5fbc
Jose Vinicius <jose.vinicius@vianuvem.com.br>
2022-09-23 20:11:02 -0300

    refactor: move DbAuthentication protocols to a single file

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,8 +1,11 @@
-import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
-import { HashComparer } from '../../protocols/criptography/hash-comparer'
-import { TokenGenerator } from '../../protocols/criptography/token-generator'
-import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
-import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
+import { 
+  Authentication,
+  AuthenticationModel,
+  TokenGenerator, 
+  HashComparer, 
+  LoadAccountByEmailRepository, 
+  UpdateAccessTokenRepository 
+} from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
@@ -15,7 +18,7 @@ export class DbAuthentication implements Authentication {
     hashComparer: HashComparer,
     tokenGenerator: TokenGenerator,
     updateAccessTokenRepository: UpdateAccessTokenRepository
-    ) {
+  ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
     this.tokenGenerator = tokenGenerator
