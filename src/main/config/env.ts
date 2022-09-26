6f2e1390a58c3a4475dfd6997d564da112fd18a9
Jose Vinicius <jviniciusoliveira@gmail.com>
2022-09-25 21:05:29 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,5 +4,6 @@ dotEnv.config()
 
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017',
-  port: process.env.PORT || 3002
+  port: process.env.PORT || 3002,
+  jwtSecret: process.env.JWT_SECRET || 'trs8dsTW'
 }
