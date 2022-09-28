57bfa64b5b32b402c28c001bf22d51426b895606
Jose Vinicius <jviniciusoliveira@gmail.com>
2022-09-28 20:54:24 -0300

    feat: ensure AuthMiddleware returns 403 if no headers are found

---
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -0,0 +1,13 @@
+import { HttpRequest } from '../protocols'
+import { AuthMiddleware } from './auth-middleware'
+import { AccessDeniedError } from '../errors'
+import { forbidden } from '../helpers/http/http-helper'
+
+describe('Auth Middleware', () => {
+  test('should return 403 if no x-access-token exists in headers', async () => {
+    const sut = new AuthMiddleware()
+    const httpRequest: HttpRequest = { }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+})
