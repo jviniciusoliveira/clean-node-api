9f97205ade913d7b8ae0ff6c1742cba72505fe9e
Jose Vinicius <jviniciusoliveira@gmail.com>
2022-09-28 19:43:26 -0300

    feat: ensure SurveyMongoRepository add a new survey

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,42 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { SurveyMongoRepository } from './survey-mongo-repository'
+
+const makeSut = (): SurveyMongoRepository => {
+  return new SurveyMongoRepository()
+}
+
+let surveyColletion: Collection
+
+describe('Survey Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyColletion = await MongoHelper.getColletion('surveys')
+    await surveyColletion.deleteMany({})
+  })
+
+  test('should add a survey on success', async () => {
+    const sut = makeSut()
+    await sut.add({
+      question: 'any_question',
+      answers: [
+        {
+          image: 'any_image',
+          answer: 'any_answer'
+        },
+        {
+          answer: 'any_answer'
+        }
+      ]
+    })
+    const survey = await surveyColletion.findOne({ question: 'any_question' })
+    expect(survey).toBeTruthy()
+  })
+})
