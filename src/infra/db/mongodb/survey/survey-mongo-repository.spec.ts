21fad9fee37c7d30a743a2a837675df1c79119c7
Jose Vinicius <jviniciusoliveira@gmail.com>
2022-10-03 19:02:22 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -22,22 +22,49 @@ describe('Survey Mongo Repository', () => {
     await surveyColletion.deleteMany({})
   })
 
-  test('should add a survey on success', async () => {
-    const sut = makeSut()
-    await sut.add({
-      question: 'any_question',
-      answers: [
-        {
+  describe('add()', () => {
+    test('should add a survey on success', async () => {
+      const sut = makeSut()
+      await sut.add({
+        question: 'any_question',
+        answers: [
+          {
+            image: 'any_image',
+            answer: 'any_answer'
+          },
+          {
+            answer: 'any_answer'
+          }
+        ],
+        date: new Date()
+      })
+      const survey = await surveyColletion.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
+    })
+  })
+
+  describe('loadAll()', () => {
+    test('should load all surveys on success', async () => {
+      await surveyColletion.insertMany([{
+        question: 'any_question',
+        answers: [{
           image: 'any_image',
           answer: 'any_answer'
-        },
-        {
-          answer: 'any_answer'
-        }
-      ],
-      date: new Date()
+        }],
+        date: new Date()
+      }, {
+        question: 'other_question',
+        answers: [{
+          image: 'other_image',
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      }])
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('other_question')
     })
-    const survey = await surveyColletion.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
   })
 })
