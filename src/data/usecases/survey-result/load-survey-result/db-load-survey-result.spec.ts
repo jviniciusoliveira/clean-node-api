35746fc6f3bfce2f607346ffdcb5bf7428ee2a87
Jose Vinicius <jviniciusoliveira@gmail.com>
2022-10-21 19:00:24 -0300

    feat: ensure DbLoadSurveyResult calls repository with correct param

---
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -0,0 +1,37 @@
+import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { mockSurveyResultModel } from '@/domain/test'
+import { DbLoadSurveyResult } from './db-load-survey-result'
+
+const mockLoadSurveyRepository = (): LoadSurveyResultRepository => {
+  class LoadSurveyResultRespositoryStub implements LoadSurveyResultRepository {
+    async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+      return Promise.resolve(mockSurveyResultModel())
+    }
+  }
+  return new LoadSurveyResultRespositoryStub()
+}
+
+type SutTypes = {
+  sut: DbLoadSurveyResult
+  loadSurveyResultRepositoryStub: LoadSurveyResultRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyResultRepositoryStub = mockLoadSurveyRepository()
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+
+  return {
+    sut,
+    loadSurveyResultRepositoryStub
+  }
+}
+
+describe('DbLoadSurveyResult UseCase', () => {
+  test('should call LoadSurveyResultRepository', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
+    const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
+    await sut.load('any_surveyId')
+    expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_surveyId')
+  })
+})
