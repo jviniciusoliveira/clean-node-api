ca17f040cb975b3b792cd7dbee34ebe2b0e135aa
Jose Vinicius <jviniciusoliveira@gmail.com>
2022-10-21 19:18:26 -0300

    feat: ensure LoadSurveyResultRepository loads surveyResult correct

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -19,9 +19,11 @@ const makeSurvey = async (): Promise<SurveyModel> => {
     question: 'any_question',
     answers: [{
       image: 'any_image',
-      answer: 'any_answer'
+      answer: 'any_answer_1'
     }, {
-      answer: 'other_answer'
+      answer: 'any_answer_2'
+    }, {
+      answer: 'any_answer_3'
     }],
     date: new Date()
   })
@@ -103,6 +105,47 @@ describe('Survey Mongo Repository', () => {
       expect(surveyResult.answers[0].percent).toBe(100)
       expect(surveyResult.answers[1].count).toBe(0)
       expect(surveyResult.answers[1].percent).toBe(0)
+    })
+  })
+  describe('loadBySurveyId()', () => {
+    test('should load survey result', async () => {
+      const account = await makeAccount()
+      const survey = await makeSurvey()
+      await surveyResultColletion.insertMany([
+        {
+          accountId: new ObjectId(account.id),
+          surveyId: new ObjectId(survey.id),
+          answer: survey.answers[0].answer,
+          date: new Date()
+        }, {
+          accountId: new ObjectId(account.id),
+          surveyId: new ObjectId(survey.id),
+          answer: survey.answers[0].answer,
+          date: new Date()
+        }, {
+          accountId: new ObjectId(account.id),
+          surveyId: new ObjectId(survey.id),
+          answer: survey.answers[1].answer,
+          date: new Date()
+        }, {
+          accountId: new ObjectId(account.id),
+          surveyId: new ObjectId(survey.id),
+          answer: survey.answers[1].answer,
+          date: new Date()
+        }
+      ])
+
+      const sut = makeSut()
+      const surveyResult = await sut.loadBySurveyId(survey.id)
+
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.surveyId).toEqual(survey.id)
+      expect(surveyResult.answers[0].count).toBe(2)
+      expect(surveyResult.answers[0].percent).toBe(50)
+      expect(surveyResult.answers[1].count).toBe(2)
+      expect(surveyResult.answers[1].percent).toBe(50)
+      expect(surveyResult.answers[2].count).toBe(0)
+      expect(surveyResult.answers[2].percent).toBe(0)
     })
   })
 })
--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -1,10 +1,11 @@
+import { ObjectId } from 'mongodb'
+import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
 import { SaveSurveyResultRepository } from '@/data/protocols/db/survey-result/save-survey-result-repository'
 import { SurveyResultModel } from '@/domain/models/survey-result'
 import { SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result'
-import { ObjectId } from 'mongodb'
 import { MongoHelper, QueryBuilder } from '../helpers'
 
-export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository, LoadSurveyResultRepository {
   async save (data: SaveSurveyResultParams): Promise<SurveyResultModel> {
     const surveyResultsCollection = await MongoHelper.getColletion('surveyResults')
     await surveyResultsCollection.findOneAndUpdate({
@@ -23,7 +24,7 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
     return surveyResult
   }
 
-  private async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+  async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
     const surveyResultsCollection = await MongoHelper.getColletion('surveyResults')
 
     const query = new QueryBuilder()
